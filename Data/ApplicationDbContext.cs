2cbcd209915186cacc464467aac4986c5ec4258b
AylinDiazA25 <aylinyamilee@gmail.com>
2022-09-03 11:13:36 -0500

    heladeriaTAMS

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using heladeriaTAMS.Models;
+
+namespace heladeriaTAMS.Data;
+
+
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Contacto> DataContactos { get; set; }
+}
